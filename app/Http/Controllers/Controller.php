e2a8a86058059f2eb611574c44d266b1a9ced7e2
musqdp <musq.dp@gmail.com>
2022-04-11 12:44:49 +0300

    first commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    /**
+     * @OA\OpenApi(
+     *     @OA\Info(
+     *         version="1.0",
+     *         title="Todo List Api",
+     *         description="Demo Todo List Api",
+     *     )
+     * ),
+     *      @OA\SecurityScheme(
+     *           securityScheme="bearerAuth",
+     *           in="header",
+     *           name="Authorization",
+     *           type="http",
+     *           scheme="Bearer",
+     *           bearerFormat="JWT",
+     *      )
+     */
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
